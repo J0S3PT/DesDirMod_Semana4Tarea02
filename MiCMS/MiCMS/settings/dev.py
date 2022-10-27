88b247d0fd0aeb8a3aed774ec939983fb0e8067f
José Plúas <jose.pluas@unl.edu.ec>
2022-10-26 22:13:12 -0500

    CArga Inicial

---
+++ MiCMS/MiCMS/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-dy&76f#d=%n1m*@wi+=f09ndv6=9k)em@m#v8o(7zc5w+&5kr@"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
